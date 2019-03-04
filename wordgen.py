93ae822d18c62ce00b739e39b3d8cc61b1ca318d
Esrelith <ad52cards@gmail.com>
2019-03-04 11:04:39 -0600

    cleaned up/improved gen()
    gen() now defaults to empty string, but can be passed a starting letter/string

--- wordgen.py
+++ wordgen.py
@@ -2,27 +2,30 @@ from text_break import text_break
 import random
 
 letters = text_break()
-letters.set_file("shakespeare.txt")
+letters.set_file("1.txt")
 letters.set_chars("qwertyuiopasdfghjklzxcvbnm ")
 letters.run()
 l_freq = letters.get_letter_freq()
 print("analysis done")
 
-def gen(start = "w", length = 8):
-    word = ""
+def gen(word = ""):
+    
+    word = word.lower()
+    print(word)
+    
+    if word == "":
+        
+        p = random.random()
+        cumulative_p = 0
 
-    p = random.random()
-    cumulative_p = 0
+        for s_letter in l_freq[" "]:
+            cumulative_p += l_freq[" "][s_letter]
 
-    for s_letter in l_freq[" "]:
-        cumulative_p += l_freq[" "][s_letter]
-
-        if p <= cumulative_p:
-            word += s_letter
-            break
+            if p <= cumulative_p:
+                word += s_letter
+                break
 
     while word[-1] != " ":
-##  while len(word) < length:
         p = random.random()
         cumulative_p = 0
         
