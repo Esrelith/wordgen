3c60609e0ecc4a95bde48538645ecc06c11aedd6
Esrelith <ad52cards@gmail.com>
2019-03-04 10:45:39 -0600

    updated file names, fixed letter selection

--- wordgen.py
+++ wordgen.py
@@ -2,27 +2,30 @@ from text_break import text_break
 import random
 
 letters = text_break()
-letters.set_file("words.txt")
+letters.set_file("shakespeare.txt")
 letters.set_chars("qwertyuiopasdfghjklzxcvbnm ")
 letters.run()
 l_freq = letters.get_letter_freq()
 print("analysis done")
 
-def gen(start, length):
-    word = start
+def gen(start = "w", length = 8):
+    word = ""
 
     p = random.random()
     cumulative_p = 0
 
-##    for s_letter in l_freq[" "]:
-##        c_p += l_freq[" "][s_letter]
-##
-##        if p <= c_p:
-##            word += s_letter
-##            break
+    for s_letter in l_freq[" "]:
+        cumulative_p += l_freq[" "][s_letter]
 
-    #while word[-1] != " " or len(word) < length:
-    while len(word) < length:
+        if p <= cumulative_p:
+            word += s_letter
+            break
+
+    while word[-1] != " ":
+##  while len(word) < length:
+        p = random.random()
+        cumulative_p = 0
+        
         for s_letter in l_freq[word[-1]]:
             cumulative_p += l_freq[word[-1]][s_letter]
 
