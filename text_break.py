bfa3c10c5ab211f56203230d0095a31699f1e7cf
Esrelith <ad52cards@gmail.com>
2019-03-04 13:36:14 -0600

    Added comments and word length counting
    added comments to most function describing function, added run_word_len to create a dictionary that counts the occurrences of word length n for figuring out whether a word should terminate. This will hopefully remove the reliance on spaces to terminate words

--- text_break.py
+++ text_break.py
@@ -2,12 +2,20 @@ import random
 
 class text_break(object):
 
-    def __init__(self):
-        self.allowed_char = None #allowed characters for word generation
+    def __init__(self, tfile = None, chars = None):
+        self.allowed_char = chars #allowed characters for word generation
         self.letter_freq = {} #dictionary of dictionaries that contain letter frequencies
-        self.letter_perc = {}
-        self.file = None #file to operate on
+        self.file = tfile #file to operate on
         self.word_length = {} #dictionary containing counts of word lengths
+
+        if self.file != None:
+            with open(self.file) as f:
+                self.contents = f.read()
+                self.contents = self.contents.lower()
+                
+        else:
+            self.contents = None
+            
 
     def set_file(self, filename):
         self.file = filename
@@ -24,10 +32,10 @@ class text_break(object):
     def get_letter_freq(self):
         return self.letter_freq
 
-##    def get_letter_perc(self):
-##        return self.letter_perc
+    def get_word_len(self):
+        return self.word_length
 
-    def __histogram(self, contents, allowed_chars):
+    def __histogram(self, contents, allowed_chars): #creates dictionary within dictionary for each letter in allowed_char
         chars = {}
         for char in contents:
             if char in allowed_chars and char not in chars.keys():
@@ -35,7 +43,7 @@ class text_break(object):
 
         return chars
 
-    def __next_letter(self, contents, allowed_chars, chars):
+    def __next_letter(self, contents, allowed_chars, chars): #add the count of letter y following letter x to letter x's dictionary
         for key in chars.keys():
             for i in range(len(contents) - 1):
                 if contents[i + 1] in allowed_chars and contents[i] == key:
@@ -47,25 +55,45 @@ class text_break(object):
 
         return chars
 
-    def __letter_percentages(self, chars):
+    def __letter_percentages(self, chars): #changes letter y's count to a percentage of the total
         for key in chars.keys():
             total = 0
-            for letter in chars[key].keys():
+            
+            for letter in chars[key].keys(): #adding up total for each letters subsequent letters
                 total += chars[key][letter]
 
-            for letter in chars[key].keys():
+            for letter in chars[key].keys(): #calculating percentage of total for each letter
                 chars[key][letter] = chars[key][letter]/total
 
         return chars
-            
+
+    def __word_break(self): #breaks contents into a list of words and makes sure word only contain allowed letters
+        lines = self.contents.split("\n")
+        words = []
+
+        for line in lines:
+            for word in line.split(" "):
+                cleaned_word = ""
+                for char in word:
+                    if char in self.allowed_char:
+                        cleaned_word += char
+                        
+                words.append(cleaned_word)
+
+        return words
+
+    def run_word_len(self): #counts amount of word length n and adds it to n's value in self.word_length
+        words = self.__word_break()
+
+        for word in words:
+            if len(word) in self.word_length.keys():
+                self.word_length[len(word)] += 1
+
+            else:
+                self.word_length[len(word)] = 1
 
     def run(self):
-        f = open(self.file)
-        contents = f.read()
-        f.close()
 
-        contents = contents.lower()
-
-        self.letter_freq = self.__histogram(contents, self.allowed_char)
-        self.letter_freq = self.__next_letter(contents, self.allowed_char, self.letter_freq)
-        self.letter_perc = self.__letter_percentages(self.letter_freq)
+        self.letter_freq = self.__histogram(self.contents, self.allowed_char)
+        self.letter_freq = self.__next_letter(self.contents, self.allowed_char, self.letter_freq)
+        self.letter_freq = self.__letter_percentages(self.letter_freq)
--- wordgen.py
+++ wordgen.py
@@ -1,9 +1,7 @@
 from text_break import text_break
 import random
 
-letters = text_break()
-letters.set_file("1.txt")
-letters.set_chars("qwertyuiopasdfghjklzxcvbnm ")
+letters = text_break("1.txt", "qwertyuiopasdfghjklzxcvbnm ")
 letters.run()
 l_freq = letters.get_letter_freq()
 print("analysis done")
@@ -13,27 +11,27 @@ def gen(word = ""):
     word = word.lower()
     print(word)
     
-    if word == "":
+    if word == "": #checks if user gave starting letter(s)
         
-        p = random.random()
+        p = random.random() #initial random generation
         cumulative_p = 0
 
-        for s_letter in l_freq[" "]:
+        for s_letter in l_freq[" "]: #picks word start based on letters coming after a space
             cumulative_p += l_freq[" "][s_letter]
 
             if p <= cumulative_p:
                 word += s_letter
                 break
 
-    while word[-1] != " ":
-        p = random.random()
+    while word[-1] != " ": #while the word hasn't ended
+        p = random.random() 
         cumulative_p = 0
         
-        for s_letter in l_freq[word[-1]]:
+        for s_letter in l_freq[word[-1]]: #picks a letter using the probalities of letters following that letter
             cumulative_p += l_freq[word[-1]][s_letter]
 
-            if p <= cumulative_p:
+            if p <= cumulative_p: #checks if letter should be added
                 word += s_letter
-                #print("letter added")
                 break
+            
     return word
